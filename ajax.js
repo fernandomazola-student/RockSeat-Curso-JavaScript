d229174d41662c64cafed09361504886d9a8c841
Fernando Mazola <fernando_mazola@hotmail.com>
2019-07-19 12:58:14 -0300

    Requisição com ajax

--- ajax.js
+++ ajax.js
@@ -42,7 +42,7 @@
 
 axios.get('https://api.github.com/users/diego3g')
     .then(function(response) {
-        console.log(response);
+        console.log(response.data.avatar_url);
     })
     .catch(function(error) {
         console.warn(error);
